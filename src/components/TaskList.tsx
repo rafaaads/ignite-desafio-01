3587cc8f2661e96b0fc497f19020a81a3293e6dc
rafaaads <rafaaads@gmail.com>
2021-04-20 21:26:23 -0300

    :tada Desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if ( !newTaskTitle ) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldTaks => [...oldTaks,newTask]);
+     
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ?  {
+        ...task,
+        isComplete: !task.isComplete
+      } : task);
+      setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(filterById => filterById.id !== id));
+    console.log(tasks);
   }
 
   return (
